87b4cc6d5e1314f5d8e54396f15334fe627c29b1
AZIZ ghizlane <ghizlaneaziz32@gmail.com>
2023-05-19 15:32:23 +0200

    population concert

--- copy.sql
+++ copy.sql
@@ -1,5 +1,7 @@
-COPY users (u_type, username, email_address)
-FROM '/home/baptiste/random/projet_bdd/users.csv' CSV HEADER;
+SET datestyle TO iso, dmy;
 
-COPY friends (id_f1, id_f2)
-FROM '/home/baptiste/random/projet_bdd/friendship.csv' CSV HEADER
+\COPY users (u_type, username, email_address) FROM '/home/popos/Documents/projet_bddssh/users.csv' CSV HEADER;
+
+\COPY concerts (host_id,c_name,price,c_date,nb_seat,nb_participant,c_cause,v_need) FROM '/home/popos/Documents/projet_bddssh/concerts.csv' CSV HEADER;
+
+\COPY friends (id_f1, id_f2) FROM '/home/popos/Documents/projet_bddssh/friendship.csv' CSV HEADER
